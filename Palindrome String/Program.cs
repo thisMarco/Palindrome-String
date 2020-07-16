ce0ca1eb60ed330db07e2e01210f253ff546c90e
Marco Picchillo <m.picchillo@yahoo.com>
2020-07-16 20:13:11 +0100

    Completed

--- Palindrome String/Program.cs
+++ Palindrome String/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Palindrome_String
 {
@@ -10,6 +6,61 @@ namespace Palindrome_String
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo userAnswer;
+            bool caseSensitive = false;
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Do you want to Enable the Case Sentitive? [Y or N] ");
+                userAnswer = Console.ReadKey();
+                if(userAnswer.Key == ConsoleKey.Y)
+                {
+                    caseSensitive = true;
+                    Console.WriteLine("\n\nCase Sensitive > Enabled\n");
+                }
+                else
+                {
+                    caseSensitive = false;
+                    Console.WriteLine("\n\nCase Sensitive > Disabled\n");
+                }
+                
+                Console.Write("Insert a string: ");
+                string userString = Console.ReadLine();
+
+                if(caseSensitive)
+                    Console.WriteLine("\n\n\t{0}\n\n", IsPalindrome(userString));
+                else
+                    Console.WriteLine("\n\n\t{0}\n\n", IsPalindrome(userString.ToLower()));
+
+                Console.Write("Do you want to test another string? [Y or N] ");
+                userAnswer = Console.ReadKey();
+
+            } while (userAnswer.Key == ConsoleKey.Y);
+        }
+
+        static public string IsPalindrome(string s)
+        {
+            if (s.Length > 1)
+            {
+                int loverIndex = 0;
+                int upperIndex = s.Length - 1;
+
+                while (loverIndex < upperIndex)
+                {
+                    if (s[loverIndex] == s[upperIndex])
+                    {
+                        loverIndex++;
+                        upperIndex--;
+                    }
+                    else
+                        return string.Format("The string {0} is not Palindrome.", s);
+                }
+                return string.Format("The string {0} is Palindrome.", s);
+            }
+            else
+                return string.Format("The string must be more than 1 character long.");
         }
     }
 }
